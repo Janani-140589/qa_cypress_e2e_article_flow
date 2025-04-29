5b03f0b27cee0ac92bc447de231d70254b48f3e4
Janani <jananiraja1405@gmail.com>
2025-04-29 15:27:13 +1200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
       on('task', {
         generateUser() {
           const email = faker.internet.email();
-          const randomNumber = Math.floor(Math.random(1000) * 1000);
+          const randomNumber = Math.floor(Math.random() * 1000);
           return {
             username: faker.person.firstName() + randomNumber,
             email: email.toLowerCase(),
